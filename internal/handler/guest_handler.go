0bbeb8ea87fb176540bfd79aa43c1c212cc22844
Ankit Bourasu <ankitbourasi0@gmail.com>
2026-03-11 20:28:58 +0530

    api: guest/analyze

--- internal/handler/guest_handler.go
+++ internal/handler/guest_handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/ankitbourasi0/job-portal/internal/repository"
@@ -10,4 +12,33 @@ type GuestHandler struct {
 	Repo *repository.GuestRepository
 }
 
-func (g GuestHandler) HandleAnalyzeResumeForAtsScore(w http.ResponseWriter, r *http.Request) {}
+func (g GuestHandler) HandleAnalyzeResumeForAtsScore(w http.ResponseWriter, r *http.Request) {
+	//1. Max Memory Limit
+	r.ParseMultipartForm(5 << 20) //limit, server will reject if file is greater than 5MB
+
+	//2.Get file from FORM(key should be 'resume' or something else)
+	file, header, err := r.FormFile("resume")
+	if err != nil {
+		http.Error(w, "File upload failed", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	//3. Validation - Check its actually Pdf
+	if header.Header.Get("Content-Type") != "application/pdf" {
+		http.Error(w, "Only pdf files are allowed", http.StatusUnsupportedMediaType)
+		return
+	}
+	//4. Read File content
+	content, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "File read failed", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Printf("Received file: %s, size: %d bytes\n", header.Filename, len(content))
+
+	//Send 'content' (byte slice) to PDF parse function
+
+	//Gemini API and Scoring Logic
+}
